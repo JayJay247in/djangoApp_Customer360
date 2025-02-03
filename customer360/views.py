679fbaf9704d143fa939e82ac3f1b53a74e5d7ac
JayJay247in <cj193532@gmail.com>
2025-02-03 19:26:15 +0100

    update

--- customer360/views.py
+++ customer360/views.py
@@ -16,7 +16,7 @@ def create_customer(request):
         phone = request.POST["phone"]
         address = request.POST["address"]
         social_media = request.POST["social_media"]
-        customer = Customer.objects.create(name=name,email=email,phone=phone,address=address)
+        customer = Customer.objects.create(name=name,email=email,phone=phone,address=address,social_media=social_media)
         customer.save()
         msg = "Successfully Saved a Customer"
         return render(request,"add.html",context={"msg":msg})
